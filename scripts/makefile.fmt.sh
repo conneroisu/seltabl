ad81d2c0c82b66c64e286fc486dafbaeecb202ff
connero <88785126+conneroisu@users.noreply.github.com>
2024-06-03 14:03:31 -0500

    latest

--- scripts/makefile.fmt.sh
+++ scripts/makefile.fmt.sh
@@ -18,17 +18,17 @@ targets=(
 
 # For each of the targets, run gofmt and goline.
 for target in "${targets[@]}"; do
-	if ls $target &>/dev/null; then
+	if ls "$target" &>/dev/null; then
 		if ! command -v gum &>/dev/null; then
 			echo "formatting $target with gofmt"
-			gofmt -w $target
+			gofmt -w "$target"
 			echo "formatting $target with golines"
-			goline -w --max-len=79 $target
+			goline -w --max-len=79 "$target"
 		else
 			gum spin --spinner dot --title "Formatting Go Files with 'go fmt' in $target" --show-output -- \
-				go fmt $target
+				go fmt "$target"
 			gum spin --spinner dot --title "Formatting Go Files with 'golines' in $target" --show-output -- \
-				golines -w --max-len=79 $target
+				golines -w --max-len=79 "$target"
 		fi
 	else
 		continue

ca047705cb932856fbcf0672256680356a7aa5ed
connero <88785126+conneroisu@users.noreply.github.com>
2024-06-05 20:29:26 -0500

    attempt add coverage

--- cmd/cmds/gen.go
+++ cmd/cmds/gen.go
@@ -6,18 +6,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// NewGenCmd returns a new cobra command for the gen subcommand
 func NewGenCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "gen",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-		Run: func(cmd *cobra.Command, args []string) {
+		Short: "Generates a scraper utilizing seltabl",
+		Long: `
+Subcommand to generate a scraper with a given file name.
+		
+Usage: 
+		
+	$ seltabl gen
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("gen called")
+			return nil
 		},
 	}
 }

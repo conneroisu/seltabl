25450d0d8f72292436181ffcbb77c22109cfa653
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-06-30 08:59:41 -0500

    add concurrent safe write response

--- tools/seltabls/cmd/cmds/bobs.go
+++ tools/seltabls/cmd/cmds/bobs.go
@@ -7,38 +7,45 @@ import (
 
 	"github.com/conneroisu/seltabl/tools/seltabls/pkg/analysis"
 	"github.com/conneroisu/seltabl/tools/seltabls/pkg/rpc"
+	"golang.org/x/sync/errgroup"
 )
 
 // WriteResponse writes a message to the writer
 func WriteResponse(
-	_ context.Context,
+	ctx context.Context,
 	state *analysis.State,
 	writer *io.Writer,
 	msg rpc.MethodActor,
 ) error {
-	reply, err := rpc.EncodeMessage(msg)
-	if err != nil {
-		return fmt.Errorf(
-			"failed to encode response to request (%s): %w",
-			msg.Method(),
-			err,
-		)
-	}
-	state.Logger.Printf("sending response (%s): %s", msg.Method(), string(reply))
-	res, err := (*writer).Write([]byte(reply))
-	if err != nil {
-		return fmt.Errorf(
-			"failed to encode response to request (%s): %w",
-			msg.Method(),
-			err,
-		)
-	}
-	if res != len(reply) {
-		return fmt.Errorf(
-			"failed to write all of response to request (%s): %w",
-			msg.Method(),
-			err,
-		)
-	}
-	return nil
+	responseCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	eg, ctx := errgroup.WithContext(responseCtx)
+	eg.Go(func() error {
+		reply, err := rpc.EncodeMessage(msg)
+		if err != nil {
+			return fmt.Errorf(
+				"failed to encode response to request (%s): %w",
+				msg.Method(),
+				err,
+			)
+		}
+		state.Logger.Printf("sending response (%s): %s", msg.Method(), string(reply))
+		res, err := (*writer).Write([]byte(reply))
+		if err != nil {
+			return fmt.Errorf(
+				"failed to encode response to request (%s): %w",
+				msg.Method(),
+				err,
+			)
+		}
+		if res != len(reply) {
+			return fmt.Errorf(
+				"failed to write all of response to request (%s): %w",
+				msg.Method(),
+				err,
+			)
+		}
+		return nil
+	})
+	return eg.Wait()
 }

326689fe2a331f427524177902f2442e5fa80e7b
Conner Ohnesorge <conneroisu@outlook.com>
2024-07-03 12:54:19 -0400

    latest

--- tools/seltabls/domain/doc.go
+++ tools/seltabls/domain/doc.go
@@ -1,2 +1,4 @@
 // Package domain provides a set of types and functions for working with the domain.
+//
+// It includes types for the domain such as prompts, responses, configurations, and errors.
 package domain

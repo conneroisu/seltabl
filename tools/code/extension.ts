2bf9421eaade8279fa392491b01d988a97cf38af
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-07-08 07:05:04 -0500

    asdfs

--- tools/code/extension.ts
+++ tools/code/extension.ts
@@ -1,33 +1,34 @@
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 function activate(context) {
-    const serverOptions: ServerOptions = {
-        command: "seltabls lsp",
-        transport: TransportKind.stdio,
-        documentSelector: [{ scheme: 'file', language: 'go' }],
-    };
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: 'file', language: 'go' }],
-    };
-    client = new LanguageClient(
-        'seltabls',
-        'seltabls',
-        serverOptions,
-        clientOptions
-    );
-    client.start();
+  const serverOptions: ServerOptions = {
+    command: "seltabls lsp",
+    transport: TransportKind.stdio,
+  };
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "go" }],
+  };
+  client = new LanguageClient(
+    "seltabls",
+    "seltabls",
+    serverOptions,
+    clientOptions
+  );
+  client.start();
 }
 
 function deactivate() {
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
 
-export {
-    activate,
-    deactivate
-};
+export { activate, deactivate };

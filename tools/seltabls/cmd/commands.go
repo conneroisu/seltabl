70fb3641a693717df1c9f7201844ff5ad21218ac
Conner Ohnesorge <conneroisu@outlook.com>
2024-07-10 08:40:36 -0400

    faster startup

--- tools/seltabls/cmd/commands.go
+++ tools/seltabls/cmd/commands.go
@@ -6,31 +6,42 @@ import (
 
 	"github.com/conneroisu/seltabl/tools/seltabls/cmd/cmds"
 	"github.com/conneroisu/seltabl/tools/seltabls/pkg/server"
+	"github.com/sourcegraph/conc"
 	"github.com/spf13/cobra"
 )
 
 // AddCommands adds the routes for the root command
 func AddCommands(ctx context.Context, root *cobra.Command) error {
-	root.AddCommand(cmds.NewVetCmd(
-		ctx,
-		os.Stdout,
-		os.Stdin,
-	))
-	root.AddCommand(cmds.NewLSPCmd(
-		ctx,
-		os.Stdout,
-		os.Stdin,
-		server.HandleMessage,
-	))
-	root.AddCommand(cmds.NewCompletionCmd(
-		ctx,
-		os.Stdout,
-		os.Stdin,
-	))
-	root.AddCommand(cmds.NewGenerateCmd(
-		ctx,
-		os.Stdout,
-		os.Stdin,
-	))
+	wg := conc.WaitGroup{}
+	wg.Go(func() {
+		root.AddCommand(cmds.NewVetCmd(
+			ctx,
+			os.Stdout,
+			os.Stdin,
+		))
+	})
+	wg.Go(func() {
+		root.AddCommand(cmds.NewLSPCmd(
+			ctx,
+			os.Stdout,
+			os.Stdin,
+			server.HandleMessage,
+		))
+	})
+	wg.Go(func() {
+		root.AddCommand(cmds.NewCompletionCmd(
+			ctx,
+			os.Stdout,
+			os.Stdin,
+		))
+	})
+	wg.Go(func() {
+		root.AddCommand(cmds.NewGenerateCmd(
+			ctx,
+			os.Stdout,
+			os.Stdin,
+		))
+	})
+	wg.Wait()
 	return nil
 }

48853b49c66ddaa66fc5d629ecc3b10cb3e15d7d
conneroisu <conneroisu@outlook.com>
2024-07-19 08:48:13 -0400

    fix(selectors): update D field HTML selector to match new content area refactor(diagnostics): inline getDiagnosticsForFile logic for clarity

--- tools/seltabls/pkg/analysis/diagnostics.go
+++ tools/seltabls/pkg/analysis/diagnostics.go
@@ -22,50 +22,42 @@ func GetDiagnosticsForFile(
 		case <-ctx.Done():
 			return nil, nil
 		default:
-			return getDiagnosticsForFile(ctx, text, data)
-		}
-	}
-}
-
-func getDiagnosticsForFile(
-	ctx context.Context,
-	text *string,
-	data parsers.StructCommentData,
-) ([]lsp.Diagnostic, error) {
-	sts, err := parsers.ParseStructs(ctx, []byte(*text))
-	if err != nil {
-		return nil, fmt.Errorf(
-			"failed to parse structs: %w",
-			err,
-		)
-	}
-	eg := errgroup.Group{}
-	var diags []lsp.Diagnostic
-	for _, st := range sts {
-		eg.Go(func() error {
-			content, err := http.DefaultClientGet(data.URLs[0])
+			sts, err := parsers.ParseStructs(ctx, []byte(*text))
 			if err != nil {
-				return fmt.Errorf(
-					"failed to get the content of the url: %w",
+				return nil, fmt.Errorf(
+					"failed to parse structs: %w",
 					err,
 				)
 			}
-			ds, err := st.Verify(ctx, data.URLs[0], content)
-			if err != nil {
-				return fmt.Errorf(
+			eg := errgroup.Group{}
+			var diags []lsp.Diagnostic
+			for _, st := range sts {
+				eg.Go(func() error {
+					content, err := http.DefaultClientGet(data.URLs[0])
+					if err != nil {
+						return fmt.Errorf(
+							"failed to get the content of the url: %w",
+							err,
+						)
+					}
+					ds, err := st.Verify(ctx, data.URLs[0], content)
+					if err != nil {
+						return fmt.Errorf(
+							"failed to get diagnostics for struct: %w",
+							err,
+						)
+					}
+					diags = append(diags, ds...)
+					return nil
+				})
+			}
+			if err := eg.Wait(); err != nil {
+				return nil, fmt.Errorf(
 					"failed to get diagnostics for struct: %w",
 					err,
 				)
 			}
-			diags = append(diags, ds...)
-			return nil
-		})
+			return diags, nil
+		}
 	}
-	if err := eg.Wait(); err != nil {
-		return nil, fmt.Errorf(
-			"failed to get diagnostics for struct: %w",
-			err,
-		)
-	}
-	return diags, nil
 }

259ba0c447b648b597262fc97fc2a19faed54109
conneroisu <conneroisu@outlook.com>
2024-07-23 09:49:19 -0400

    fix(ncaa): remove unnecessary `must` attribute from NCAATeamCode struct feat(set): add reduceHTML function to filter selection by text fix(set): refactor cfg.MustBePresent check to use reduceHTML function chore(set): add missing import for strings package refactor(testdata): remove `must` attribute from testdata/example.go struct fix(slices): optimize cfg.MustBePresent handling by reducing HTML node selection chore(set): update setFieldValue function with error handling for unsupported types style(ncaa): improve code readability by removing redundant attribute chore(slices): remove redundant text extraction in cfg.MustBePresent check

--- tools/seltabls/testdata/example.go
+++ tools/seltabls/testdata/example.go
@@ -2,6 +2,6 @@ package testdata
 
 // @url: https://stats.ncaa.org/game_upload/team_codes
 type TeamCode struct {
-	ID   int    `json:"id"   seltabl:"ID"   hSel:"tr.grey_heading td:nth-child(1)" dSel:"tr:not(.grey_heading):not(.heading) td:nth-child(1)" ctl:"text" must:"NCAA Codes"`
+	ID   int    `json:"id"   seltabl:"ID"   hSel:"tr.grey_heading td:nth-child(1)" dSel:"tr:not(.grey_heading):not(.heading) td:nth-child(1)" ctl:"text"`
 	Name string `json:"name" seltabl:"Name" hSel:"tr.grey_heading td:nth-child(2)" dSel:"tr:not(.grey_heading):not(.heading) td:nth-child(2)" ctl:"text"`
 }

e2f732c5d661188fcf4a812e3410d205130aba5d
conneroisu <conneroisu@outlook.com>
2024-07-31 11:01:16 -0400

    fix(browser): change OpenBrowser to return an error
    refactor(browser): remove unnecessary log package import
    
    fix(browser): handle unsupported platforms with an error return
    
    refactor(browser): return errors immediately in OpenBrowser function
    
    style(browser): omit error variable declaration in OpenBrowser
    
    fix(browser): remove redundant variable declaration in OpenBrowser
    
    chore(browser): update OpenBrowser function signatures for consistency
    
    enhance(browser): improve error handling in OpenBrowser
    
    refactor(browser): simplify OpenBrowser function by reducing control flow
    
    fix(browser): adjust OpenBrowser to specific runtime detection

--- tools/seltabls/pkg/http/browser.go
+++ tools/seltabls/pkg/http/browser.go
@@ -2,26 +2,21 @@ package http
 
 import (
 	"fmt"
-	"log"
 	"os/exec"
 	"runtime"
 )
 
 // OpenBrowser opens the default browser with the given url
-func OpenBrowser(url string) {
-	var err error
+func OpenBrowser(url string) error {
 	switch runtime.GOOS {
 	case "linux":
-		err = exec.Command("xdg-open", url).Start()
+		return exec.Command("xdg-open", url).Start()
 	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).
+		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).
 			Start()
 	case "darwin":
-		err = exec.Command("open", url).Start()
+		return exec.Command("open", url).Start()
 	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("unsupported platform")
 	}
 }
